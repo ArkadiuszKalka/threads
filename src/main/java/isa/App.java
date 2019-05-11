5ac805b24cf9bc0de5513514253723b032d44fc6
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-05-11 20:34:12 +0200

    Task 3

--- src/main/java/isa/App.java
+++ src/main/java/isa/App.java
@@ -2,9 +2,32 @@ package isa;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
+        for (int i = 0; i < 2; i++) {
+
+            final int current = i;
+
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(current * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("T0" + (current + 1));
+            });
+            System.out.println("test0000" + i);
+            thread.start();
+
+            thread.join();
+        }
+        System.out.println("test 1");
+
+
+/*
+// Zadanie 2
         for (int i = 0; i < 8; i++) {
 
             final int current = i;
@@ -16,8 +39,10 @@ public class App {
             thread.start();
 
         }
+*/
 
 /*
+// Sposób 1
                     System.out.println("MT start");
             Thread thread = new Thread(() -> {
                 System.out.println("T0 start");
@@ -32,6 +57,6 @@ public class App {
             }
             System.out.println("MT stop");
         */
-    }
+        }
 
-}
+    }

98886aea1fbd8beccb92f017081bf5fce3fc5d28
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-05-11 20:18:28 +0200

    Task 2

--- src/main/java/isa/App.java
+++ src/main/java/isa/App.java
@@ -3,19 +3,35 @@ package isa;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("MT start");
-        Thread thread = new Thread(() -> {
-            System.out.println("T0 start");
-            for (int i = 0; i < 5; i++) {
-                System.out.println("T0 " + i);
-            }
-            System.out.println("T0 stop");
-        });
-        thread.start();
-        for (int i = 0; i < 5; i++) {
-            System.out.println("MT " + i);
+
+
+        for (int i = 0; i < 8; i++) {
+
+            final int current = i;
+
+            Thread thread = new Thread(() -> {
+                System.out.println("T0" + (current + 1));
+            });
+
+            thread.start();
+
         }
-        System.out.println("MT stop");
+
+/*
+                    System.out.println("MT start");
+            Thread thread = new Thread(() -> {
+                System.out.println("T0 start");
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("T0 " + i);
+                }
+                System.out.println("T0 stop");
+            });
+            thread.start();
+            for (int i = 0; i < 5; i++) {
+                System.out.println("MT " + i);
+            }
+            System.out.println("MT stop");
+        */
     }
 
 }
